8c44db65a191cdd5c40b571da139806f8f581efc
Sizhe <lisizhe@pku.edu.cn>
2022-04-03 15:45:40 +0800

    frame completes

--- Graph.py
+++ Graph.py
@@ -3,20 +3,23 @@ import numpy as np
 class Graph:
     def __init__(self, list_edge):
         self.list_edge = list_edge
+        self.num_nodes = np.max(list_edge) + 1
         self.graph = self.build_graph_from_edge(list_edge)
         self.in_degree = self.cal_indegree_from_edge(list_edge)
 
 
-    def build_graph_from_edge(list_edge):
-        num_nodes = np.max(list_edge) + 1
-        graph = [[] for i in range(num_nodes)]
+    def build_graph_from_edge(self, list_edge):
+        graph = [[] for i in range(self.num_nodes)]
         for edge in list_edge:
             graph[edge[0]].append(edge[1])
         return graph
 
-    def cal_indegree_from_edge(list_edge):
-        num_nodes = np.max(list_edge) + 1
-        in_degree = [0 for i in range(num_nodes)]
+    def cal_indegree_from_edge(self, list_edge):
+        in_degree = [0 for i in range(self.num_nodes)]
         for edge in list_edge:
             in_degree[edge[1]] += 1
         return in_degree
+
+    # 生成随机满足拓扑排序条件的路径
+    def generate_init_method(self):
+        raise NotImplementedError
--- abc.py
+++ abc.py
@@ -1,16 +1,62 @@
+from http.client import NOT_MODIFIED
 from NectarSource import Nectar
 from Graph import Graph
 from utils import io_util, utils
 import numpy as np
+import copy
 
 class ABC:
     def __init__(self, config):
         self.graph = Graph(io_util.load_mtx(config["graph"]["graph_path"]))
         self.num_thread = config["global"]["num_threads"]
+        self.num_bees = config["nectar"]["num_nectars"]
+        self.num_loops = config["global"]["num_loop"]
+
+        self.methodGroup = []
+        self.employedBees = []
+        self.OnlookerBees = []
+        for i in range(self.num_bees):
+            method = Nectar(self.graph.generate_init_method, 0)
+            self.methodGroup.append(copy.deepcopy(method))
+            self.employedBees.append(copy.deepcopy(method))
+            self.OnlookerBees.append(copy.deepcopy(method))
+
+        self.bestMethod = Nectar(self.graph.generate_init_method, 0)
+
+    def send_employedBees(self):
+        raise NotImplementedError
+
+    
+    def calculate_Probabilities(self):
+        raise NotImplementedError
+
+    def send_onlookerBees(self):
+        raise NotImplementedError
+    
+    def send_scoutBees(self):
+        raise NotImplementedError
+    
+    def update_best(self):
+        raise NotImplementedError
+
+    def solve(self):
+        for i in range(self.num_loops):
+            self.send_employedBees()
+            self.calculate_Probabilities()
+            self.send_onlookerBees()
+            self.update_best()
+            self.send_scoutBees()
+            self.update_best()
+        return self.bestMethod
+
 
 def main():
+
     config = io_util.load_yaml("./config.yml", True)
+    model = ABC(config)
+    bestMethod = model.solve()
     
+    print(bestMethod.cal_trueFit())
     
 
 if __name__ == "__main__":
